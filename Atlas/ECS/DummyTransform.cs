a64013de23d3d40220be9c3df9a0b486da317b02
apeltsi <49206921+apeltsi@users.noreply.github.com>
2023-07-09 22:16:37 +0300

    Dummy transform fixes

--- Atlas/ECS/DummyTransform.cs
+++ Atlas/ECS/DummyTransform.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using SolidCode.Atlas.Rendering;
 
 namespace SolidCode.Atlas.ECS;
@@ -9,6 +10,7 @@ namespace SolidCode.Atlas.ECS;
 /// </summary>
 public class DummyTransform : Transform
 {
+    
     public override void RegisterDrawable(Drawable d)
     {
         // Lets do nothing because we're a dummy
@@ -18,5 +20,30 @@ public class DummyTransform : Transform
     public override void UnregisterDrawable(Drawable d)
     {
         // Same
+    }
+    
+    public override Matrix4x4 GetTransformationMatrix()
+    {
+        Vector2 pos = this.Position;
+        Vector2 scale = this.Scale;
+        float rot = this.Rotation * ((float)Math.PI / 180f);
+        Matrix4x4 scaleMat = new Matrix4x4(
+            scale.X, 0, 0, 0,
+            0, scale.Y, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        );
+        Matrix4x4 rotation = new Matrix4x4(
+            (float)Math.Cos(rot), (float)-Math.Sin(rot), 0, 0,
+            (float)Math.Sin(rot), (float)Math.Cos(rot), 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1);
+        Matrix4x4 translation = new Matrix4x4(
+            1, 0, 0, pos.X,
+            0, 1, 0, pos.Y,
+            0, 0, 1, 0,
+            0, 0, 0, 1);
+
+        return translation * rotation * scaleMat;
     }
 }
