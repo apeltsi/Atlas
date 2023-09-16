81486530f9bec9eb927118f8504cc8dac3b4671e
apeltsi <49206921+apeltsi@users.noreply.github.com>
2023-09-16 23:44:16 +0200

    Refactor AnchorPoint to only use Vector2s

--- Atlas/UI/AnchoredPoint.cs
+++ Atlas/UI/AnchoredPoint.cs
@@ -3,30 +3,55 @@ using SolidCode.Atlas.Rendering;
 
 namespace SolidCode.Atlas.UI;
 
+/// <summary>
+/// A UI anchor point
+/// </summary>
 public class AnchoredPoint
 {
+    /// <summary>
+    /// The anchor point
+    /// </summary>
     public Vector2 AnchorPoint;
-    public RelativeVector Position;
+    /// <summary>
+    /// The position of the anchor point
+    /// </summary>
+    public Vector2 Position;
+    /// <summary>
+    /// The parent of the anchor point
+    /// </summary>
     public RectTransform? Parent;
-    private Vector2 cachedPositon;
-    private double cacheTime;
-    public AnchoredPoint(Vector2 anchorPoint, RelativeVector position)
+    private Vector2 _cachedPosition;
+    private double _cacheTime;
+    /// <summary>
+    /// Creates a new anchor point
+    /// </summary>
+    /// <param name="anchorPoint">The anchor point value</param>
+    /// <param name="position">The position relative to the anchor point</param>
+    public AnchoredPoint(Vector2 anchorPoint, Vector2 position)
     {
         AnchorPoint = anchorPoint;
         Position = position;
     }
 
-    public AnchoredPoint(RelativeVector position)
+    /// <summary>
+    /// Creates a new anchor point at the center
+    /// </summary>
+    /// <param name="position">The position relative to the anchor point</param>
+    public AnchoredPoint(Vector2 position)
     {
         this.Position = position;
         AnchorPoint = new Vector2(0.5f);
     }
 
+    /// <summary>
+    /// Evaluates the anchor point
+    /// </summary>
+    /// <returns>The evaluated position</returns>
     public Vector2 Evaluate()
     {
-        if (Time.time == cacheTime)
-            return cachedPositon;
-        Vector2 ret = Position.Evaluate() + (Parent?.Position.Evaluate() ?? Vector2.Zero);
+        if (Time.time == _cacheTime)
+            return _cachedPosition;
+        Vector2 ret = Position + (Parent?.Position.Evaluate() ?? Vector2.Zero);
         if (Parent == null)
         {
             ret += (AnchorPoint * 2f - new Vector2(1f)) * Renderer.UnitScale;
@@ -36,8 +61,8 @@ public class AnchoredPoint
             ret += (AnchorPoint * 2f - new Vector2(1f)) * Parent.Scale.Evaluate();
         }
         
-        cacheTime = Time.time;
-        cachedPositon = ret;
+        _cacheTime = Time.time;
+        _cachedPosition = ret;
 
         return ret;
     }
